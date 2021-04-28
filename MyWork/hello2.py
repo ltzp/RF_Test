0775344943a14bd8fc53e46d763800838aaf56cc
yuetao <806518802@qq.com>
2021-04-28 22:22:26 +0800

    打包测试1

---
+++ MyWork/hello2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/04/28
+# @Author  : yuetao
+# @Site    : 
+# @File    : hello2.py
+# @Desc    :
+from MyLibrary.my_operation import MyOperation
+
+
+if __name__ == '__main__':
+    print(MyOperation().add(3, 4))
